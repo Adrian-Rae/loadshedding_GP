94866bed3fe3afd1db77887d47801b5fb5c467d2
Adrian-Rae-19004029 <72435551+Adrian-Rae@users.noreply.github.com>
2022-03-06 11:28:17 +0200

    Population Generation Update
    Added the FULL method for population generation

--- src/GPPopulator.py
+++ src/GPPopulator.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-from GPParseTree import *
 from GPExceptions import InvalidPopulationGenerationMethodException, InvalidPopulationSizeException
+from GPParseTree import *
 
 
 class Populator(Generic[T]):
@@ -11,7 +11,8 @@ class Populator(Generic[T]):
         FULL = 2
         RAMPED = 3
 
-    def __init__(self, size: int, max_depth: int, terminal_set: TerminalSetType, operator_set: OperatorSetType, method: Method = Method.GROW) -> None:
+    def __init__(self, size: int, max_depth: int, terminal_set: TerminalSetType, operator_set: OperatorSetType,
+                 method: Method = Method.GROW) -> None:
 
         if method is None:
             raise InvalidPopulationGenerationMethodException
@@ -27,11 +28,42 @@ class Populator(Generic[T]):
         self._operator_set = operator_set
 
     # TODO: finish FULL and RAMPED
-    def generate(self) -> list[ParseTree[T]]:
+    def generate(self) -> List[ParseTree[T]]:
+
+        # GROW METHOD
         if self._method == Populator.Method.GROW:
-            return [ParseTree.random(self._max_depth,self._terminal_set,self._operator_set) for i in range(self._size)]
+
+            # Simply return n randomly generated trees with the specified maximum depth
+            return [ParseTree.random(self._max_depth, self._terminal_set, self._operator_set) for _ in
+                    range(self._size)]
+
+        # FULL METHOD
         elif self._method == Populator.Method.FULL:
-            return []
+
+            # Generate a population of n members with identical (maximum) depth
+            population: List[ParseTree[T]] = []
+
+            while True:
+
+                # Create a new member
+                new_member: ParseTree[T] = ParseTree.random(self._max_depth, self._terminal_set, self._operator_set)
+
+                # If the depth is desired
+                if new_member.get_depth() == self._max_depth:
+
+                    # keep the member
+                    population.append(new_member)
+
+                # If the population size is reached, terminate
+                if len(population) == self._size:
+                    break
+
+            # Return the population
+            return population
+
+        # RAMPED HALF-AND-HALF
         elif self._method == Populator.Method.RAMPED:
             return []
+
+        # Invalid generation method bound
         raise InvalidPopulationGenerationMethodException
--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from GPAtom import Terminal, Operator
 from GPPopulator import Populator
 
 if __name__ == '__main__':
+
     t_set = [Terminal[int](), Terminal[int]()]
     o_set = [Operator[int](1), Operator[int](3)]
-    p = Populator(5, 2, t_set, o_set)
+    
+    p = Populator(5, 5, t_set, o_set, method=Populator.Method.FULL)
+
     for k in p.generate():
-        print(k)
+        print("Depth: {}\nTree:\n{}".format(k.get_depth(), k))
